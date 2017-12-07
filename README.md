3315af2be63d9a97f4a68958ef3a1ac1a9fe99c2
Joeffison Andrade <joeffison@gmail.com>
2017-12-07 13:58:34 -0300

    Writting some psude

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# data_engineer_flixbus
+The approach I would apply is: 
+- Break down entry in pieces that the mentioned method rank_trips() supports (up to 5) 
+- Then Build a (heap-like) tree and rearrange the nodes and combine the subtrees with new calls to the original rank_trips() method 
+
+O(n log(n))
--- data_engineer_flixbus/bustrip/views.py
+++ data_engineer_flixbus/bustrip/views.py
@@ -6,4 +6,11 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("BusTrip.")
+    return HttpResponse("""
+    The approach I would apply is:
+    <br />\n- Break down entry in pieces that the mentioned method rank_trips() supports (up to 5)
+    <br />\n- Then Build a (heap-like) tree and rearrange the nodes and combine the subtrees with new calls to the original 
+    rank_trips() method
+    <br />\n
+    <br />\nO(n log(n))
+    """)
